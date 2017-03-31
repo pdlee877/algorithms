0fd6f0e4e6ac1dd82e6bbe740ff938f3a9f10fff
Paula Lee <pdlee877@gmail.com>
2017-03-30 23:45:46 -0700

    add arrays file to go over array manipulation, add learn_python to make sure I have a better understanding of functions

---
+++ arrays.py
@@ -0,0 +1,7 @@
+#Problem: given an integer array, output all the unique pairs that sum
+# up to a specific value k
+
+# example: pair_sum([1,3,2,2],4)
+# return => (1,3) (2,2)
+
+ def pair_sum(arr, k):
