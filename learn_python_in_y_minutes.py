16bb56e9d97c7e8f13e5b42e0b24e450582061fb
Paula Lee <pdlee877@gmail.com>
2017-03-31 01:14:32 -0700

    array manipulation

--- learn_python_in_y_minutes.py
+++ learn_python_in_y_minutes.py
@@ -9,7 +9,7 @@
     this
 """
 
-# 1. Datatypes and operators
+# 1. Datatypes and Operators
 # string, integer, float, boolean
 # +, -, *, /
 
@@ -83,3 +83,62 @@ None # => None
 # This checks for equality of object identity
 "etc" is None # => False
 None is None # => True
+
+# 2. Variables and Collections
+
+# By default the print function print out a new line at the end
+# use the optional argument to change the end character
+print("Hello, World", end="!")
+# => "Hello, World!"
+
+# get input from the user -> Ruby gets
+# print("Do you like strolling through Target?") # YES
+# user_input = input()
+# print(user_input()) # => YES
+
+# if can be used as an expression
+"WOW!" if 3 > 2 else 2
+
+# to add at the end of an array use .append()
+
+arr = []
+arr.append(2)
+arr.append(4)
+# arr = [2,4]
+
+# remove from the end with .pop()
+
+arr.pop()
+# arr = [2]
+
+# ranges with slice syntax
+# arr = [1,2,3,4,5]
+
+# range but stop BEFORE index 3
+arr[1:3] # => [2,3]
+
+# start [n:] here but go all the way to the end
+arr[2:] # => [3,4,5]
+
+# start at the very beginning but end here [:n]
+arr[:3] # => [1,2,3]
+
+# every other element
+arr[::2] # => [1,3,5]
+
+# reverse order
+arr[::-1] # => [5,4,3,2,1]
+
+
+# arr[start:end:step]
+
+# makes a deep copy of an array
+# however arr is arr2 = False
+arr2 = arr[:]
+# => arr2 = [1,2,3,4,5]
+
+# remove elements
+del arr[2] # => [1,2,4,5] => 3 is removed
+
+# remove first occurrence of a value
+arr.remove(5) # => [1,2,4]
